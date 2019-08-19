beb88cd7c1989584af086b7362c27cd634f9fb1d
teeepei <teppei.nakamura@konicaminolta.com>
2019-08-19 14:08:39 +0900

    サーバアプリのお気に入りアイコンの読み込みエラー対応

--- cloud/WebApp/ec2_mount.sh
+++ cloud/WebApp/ec2_mount.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
+rm -fr ./static/zybo ./static/face
 /usr/local/bin/s3fs sas5-unei-test-zybo  /mnt/zybo -o rw,allow_other,uid=1000,gid=1000,default_acl=public-read,iam_role="sas5_ec2_s3_mount_role"
 /usr/local/bin/s3fs sas5-unei-test-face  /mnt/face -o rw,allow_other,uid=1000,gid=1000,default_acl=public-read,iam_role="sas5_ec2_s3_mount_role"
+ln -s /mnt/zybo ./static/zybo
+ln -s /mnt/face ./static/face
+
